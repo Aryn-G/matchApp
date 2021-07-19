594bb26ba4c71c2e3c047b37991b04d2efd5a6c3
Aryan Gera <aryangera@hotmail.com>
2021-07-19 09:02:01 -0400

    changed data

--- client/data/users.js
+++ client/data/users.js
@@ -3,6 +3,11 @@ let usersArr = [
     name: "BobGamer6",
     image: "https://lh3.googleusercontent.com/proxy/ySCITxbND60zxM98JtU0ioaXdKJvKT3TSLL655ACTsi74mn0SeqcqUPOHSsW0mfWVK56BJpvD2WTyAor0SZGJgoxH5GlDre_",
     score: [1, 5, 2, 3, 4, 3, 2, 5, 2, 5],
+  },  
+  {
+    name: "Mr Ia",
+    image: "https://lh3.googleusercontent.com/proxy/ySCITxbND60zxM98JtU0ioaXdKJvKT3TSLL655ACTsi74mn0SeqcqUPOHSsW0mfWVK56BJpvD2WTyAor0SZGJgoxH5GlDre_",
+    score: [5, 5, 5, 5, 5, 5, 2, 5, 2, 5],
   },
 ];
 
